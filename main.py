79ed2ea4353b2dd05a4a52748bbf8b7dae4e14f8
Marion N'dri <ndrimarion5@gmail.com>
2026-06-09 18:21:51 +0000

    Code class

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ class MyClass: # cette classe a un constructeur qui prend un argument "name" et 
     def greet(self):
         return f"Hello, {self.name}!"
 # Exemple d'utilisation de la classe MyClass
-if __name__ == "__main__":
+if __name__ == "__main__": #
     my_object = MyClass("Alice")
     print(my_object.greet())  # Output: Hello, Alice!
